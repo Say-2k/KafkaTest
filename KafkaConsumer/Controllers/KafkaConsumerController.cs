fe164fe13cbd66b24c1200158a3f993d58b3bf84
Алексей Кравцов <alexei.cr@mail.ru>
2024-01-31 00:07:39 +0300

    Добавьте файлы проекта.

---
+++ KafkaConsumer/Controllers/KafkaConsumerController.cs
@@ -0,0 +1,38 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KafkaConsumer.Controllers
+{
+    [ApiController, Route("/api/[controller]/")]
+    public class KafkaConsumerController : ControllerBase
+    {
+        private readonly ILogger<KafkaConsumerController> _logger;
+        private readonly IConsumer<Null, string> _consumer;
+
+        public KafkaConsumerController(ILogger<KafkaConsumerController> logger)
+        {
+            _logger = logger;
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = Environment.GetEnvironmentVariable("KAFKA_ADDRESS"),
+                GroupId = "demo-group",
+                AutoOffsetReset = AutoOffsetReset.Latest
+            };
+            _consumer = new ConsumerBuilder<Null, string>(config).Build();
+        }
+
+        public async Task<IActionResult> Get(CancellationToken cancellationToken)
+        {
+            _consumer.Subscribe("demo-topic");
+            string? message = null;
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var result = _consumer.Consume(cancellationToken);
+                _logger.LogInformation("Getting >> {Message}", result.Message.Value);
+                message += result.Message.Value + "\r\n";
+            }            
+            return Ok(message);
+        }
+
+    }
+}
---
+++ KafkaProducer/Controllers/KafkaProducerController.cs
@@ -0,0 +1,36 @@
+﻿using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KafkaProducer.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]/")]
+    public class KafkaProducerController : ControllerBase
+    {
+        private readonly ILogger<KafkaProducerController> _logger;
+        private readonly IProducer<Null, string> _producer;
+
+        public KafkaProducerController(ILogger<KafkaProducerController> logger)
+        {
+            _logger = logger;
+            var config = new ProducerConfig
+            {
+                BootstrapServers = Environment.GetEnvironmentVariable("KAFKA_ADDRESS")
+
+            };
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string message, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Sending >> {message}");
+
+            await _producer.ProduceAsync(
+                "demo-topic",
+                new Message<Null, string> { Value = message },
+                cancellationToken);
+            return Ok();
+        }
+    }
+}
